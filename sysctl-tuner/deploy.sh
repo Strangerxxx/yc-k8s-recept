984588824d81ad135fc1f6470eabbc86a07e3e3c
Daniel Chaplin <black.danek@gmail.com>
2020-03-01 17:28:15 +0300

    add yc-cli call to configure docker credentials

--- sysctl-tuner/deploy.sh
+++ sysctl-tuner/deploy.sh
@@ -4,6 +4,7 @@ registry_json=$(yc container registry get --name sysctl-tuner --format json || y
 registry_id=$(echo $registry_json | jq -r ".id")
 IMAGE=cr.yandex/${registry_id}/tuner:latest
 docker build -t ${IMAGE} .
+yc container registry configure-docker
 docker push ${IMAGE}
 sed "s|%IMAGE%|${IMAGE}|" sysctl-tuner-operator.tpl > sysctl-tuner-operator.yaml
 kubectl create ns sysctl-tuner
